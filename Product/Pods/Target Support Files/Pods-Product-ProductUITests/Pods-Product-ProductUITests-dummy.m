b404f54be13455a1a61396fbca5fb225ee4f8db2
waaleedkhan <waaleedkhan@gmail.com>
2021-06-04 19:52:08 +0500

    Added Swift Lint

---
+++ Product/Pods/Target Support Files/Pods-Product-ProductUITests/Pods-Product-ProductUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Product_ProductUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Product_ProductUITests
+@end
---
+++ Product/Pods/Target Support Files/Pods-ProductTests/Pods-ProductTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ProductTests : NSObject
+@end
+@implementation PodsDummy_Pods_ProductTests
+@end
