dacf590ed52ac7659471bf2b6696095fca751f65
waaleedkhan <waaleedkhan@gmail.com>
2021-06-05 21:07:33 +0500

    Thumbnail Image Loading

--- Product/Product/Cells/ProductCell/ProductCell.swift
+++ Product/Product/Cells/ProductCell/ProductCell.swift
@@ -1,6 +1,7 @@
 //  Created on 6/5/21.
 
 import UIKit
+import Combine
 
 class ProductCell: TableViewCell {
 
@@ -8,6 +9,10 @@ class ProductCell: TableViewCell {
     
     var indexPath: IndexPath?
     var model: Product?
+    
+    // Private
+    private var cancellable: AnyCancellable?
+    private var animator: UIViewPropertyAnimator?
     
     // MARK: - Outlets
     
@@ -25,8 +30,15 @@ class ProductCell: TableViewCell {
         initalize()
     }
     
+    override public func prepareForReuse() {
+         super.prepareForReuse()
+        imageViewThumbnail.image = nil
+        imageViewThumbnail.alpha = 0.0
+        animator?.stopAnimation(true)
+        cancellable?.cancel()
+     }
+    
     private func initalize() {
-        imageViewThumbnail.backgroundColor = .blue
         imageViewThumbnail.layer.cornerRadius = imageViewThumbnail.frame.size.width / 2.0
     }
 
@@ -41,6 +53,29 @@ class ProductCell: TableViewCell {
         lblName.text = model.name
         lblPrice.text = model.price
         lblCreatedAt.text = model.createdAt?.toDisplayFormat()
+        
+        if let thumbnail = model.imageUrlsThumbnails?.first, let url = URL(string: thumbnail) {
+            cancellable = loadImage(for: thumbnail, url: url).sink { [unowned self] image in self.showImage(image: image) }
+        }
+    }
+    
+    // MARK: - Image handling
+    // https://github.com/sgl0v/OnSwiftWings/blob/master/ImageCache.playground/Sources/MovieTableViewCell.swift
+    private func showImage(image: UIImage?) {
+        imageViewThumbnail.alpha = 0.0
+        animator?.stopAnimation(false)
+        imageViewThumbnail.image = image
+        animator = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: .curveLinear, animations: {
+            self.imageViewThumbnail.alpha = 1.0
+        })
+    }
+    
+    private func loadImage(for thumbnailUrl: String, url: URL) -> AnyPublisher<UIImage?, Never> {
+        return Just(thumbnailUrl)
+        .flatMap({ _ -> AnyPublisher<UIImage?, Never> in
+            return ImageLoader.shared.loadImage(from: url)
+        })
+        .eraseToAnyPublisher()
     }
     
 }
