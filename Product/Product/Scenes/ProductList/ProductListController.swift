9daab839a77417c0660d2a416b88d83f5b128708
waaleedkhan <waaleedkhan@gmail.com>
2021-06-05 17:24:03 +0500

    Initiate Root Controller from AppDelegate Window

---
+++ Product/Product/Scenes/ProductList/ProductListController.swift
@@ -0,0 +1,21 @@
+//  Created on 6/5/21.
+
+import UIKit
+
+class ProductListController: BaseController {
+
+    // MARK: - Life Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ Product/Product/Scenes/ProductList/ProductListView.swift
@@ -0,0 +1,6 @@
+//  Created on 6/5/21.
+
+import UIKit
+
+class ProductListView: BaseView {
+}
